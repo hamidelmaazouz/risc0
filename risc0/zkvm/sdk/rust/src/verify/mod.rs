244137a20a46f7faa06cb614f42371f53db91c32
nils <nils@risc0.com>
2022-08-03 20:34:50 +0000

    Add test for rust verifier

--- risc0/zkvm/sdk/rust/src/verify/mod.rs
+++ risc0/zkvm/sdk/rust/src/verify/mod.rs
@@ -11,3 +11,38 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#[cfg(test)]
+mod tests {
+    use crate::method_id::MethodId;
+    use crate::receipt::Receipt;
+    use risc0_zkp::core::sha::DIGEST_WORD_SIZE;
+    use std::{convert::TryFrom, vec::Vec};
+
+    #[test]
+    fn test_receipt() {
+        const RECEIPT_DATA: &[u8] = include_bytes!("../../benches/simple_receipt.receipt");
+        const METHOD_ID: &[u8] = include_bytes!("../../benches/simple_receipt.id");
+
+        let receipt_data: Vec<u32> = RECEIPT_DATA
+            .chunks(DIGEST_WORD_SIZE)
+            .map(|bytes| u32::from_le_bytes(<[u8; DIGEST_WORD_SIZE]>::try_from(bytes).unwrap()))
+            .collect();
+        let receipt: Receipt = crate::serde::from_slice(&receipt_data).unwrap();
+
+        let method_id = MethodId::from_slice(METHOD_ID).unwrap();
+
+        std::println!(
+            "Receipt: journal length {} seal length {}",
+            receipt.journal.len(),
+            receipt.seal.len(),
+        );
+
+        for i in 0..50 {
+            std::print!(" {}", receipt.seal[i]);
+        }
+        std::println!("\n");
+
+        receipt.verify(&method_id).unwrap();
+    }
+}
